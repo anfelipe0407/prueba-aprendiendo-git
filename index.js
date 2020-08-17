20379fbd9b1620df4d1e7b4aa3fdbf614ea4255a
anfelipe0407 <andf2019@gmail.com>
2020-08-17 17:32:22 -0500

    primer commit desde master

---
+++ index.js
@@ -0,0 +1,13 @@
+const nombre = "andres"
+
+let accion = (name) => {
+    return 'hola' + name 
+}
+
+console.log(accion(nombre))
+
+let primer_commmit = true
+
+let segundo_commit_master = true
+
+let tercer_commit_master = true
